206c48d4cbcd187b922af00a022251d118ce98d0
hyeon su <zzplokh@gmail.com>
2024-07-19 23:44:09 +0900

    프로젝트 커밋

---
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,45 @@
+package com.mysite.sbb.answer;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private LocalDateTime createDate;
+
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private SiteUser author;
+
+	@ManyToOne
+	@JoinColumn(name = "question_id")
+	private Question question;
+
+	private LocalDateTime modifyDate;
+
+	@ManyToMany
+	Set<SiteUser> voter;
+}
---
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,38 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionService questionService;
+
+	@Transactional
+
+	@Test
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03ㅇ]", i);
+
+			String content = "내용 없음";
+			this.questionService.create(subject, content, null);
+		}
+	}
+}
